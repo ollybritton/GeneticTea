b3dc6c6b538cfb268137f7b8280c13f79563d6ce
lpereyrasantiago <turingsmachine@protonmail.com>
2018-07-31 20:31:36 -0300

    mutation process perfectioned and overall improvements

--- natural_selection.py
+++ natural_selection.py
@@ -6,17 +6,20 @@ import pickle
 
 def natural_selection(population):
 
+    """Defines the new generation of a population of teas as that half of the
+    generation that has higher fitness values, i.e. higher rankings from the user."""
+
     population_size = len(population.generation)
     next_population_size = math.floor(population_size / 2)
 
     new_generation = sorted(population.generation, key=lambda x: x.rate, reverse=True)[0:next_population_size]
-    # la nueva generación serán los x especímenes con la puntuación más alta,
-    # donde x representa el tamaño de la nueva generación (x = cur_gen / 2).
 
     population.generation = new_generation
 
 
 def save_population(population):
+
+    """Saves the evaluated population as is for later use."""
 
     with open('save', 'wb') as f:
         pickle.dump(population, f)
@@ -24,6 +27,8 @@ def save_population(population):
 
 
 def load_population():
+
+    """Loads the last saved population."""
 
     with open('save', "rb") as f:
         population = pickle.load(f)
@@ -34,15 +39,21 @@ def load_population():
 
 def main():
 
+    """Main function. Offers to load a previous population or to generate a new
+    one. Makes the population subject of the evolutionary model by displaying
+    it, asking for user's rank, selecting the most fitted individuals,
+    reproducing them and mutating their offspring and displaying the new
+    generation to be ranked. Repeates the process until it boils down to
+    one remaining cup of tea.
+
+    Saves the population as is on each loop."""
+
     choise = input(Fore.GREEN + """Do you have a generation brewing already?
     Type 'y' to load the last generation or "n" to start a new one.""")
 
     if choise is "n":
-
         litter = brewery.Population()
-
     if choise is "y":
-
         litter = load_population()
 
     while True:  # while the optimal tea is not breed.
@@ -50,14 +61,13 @@ def main():
         print("\n -------- GENERATION " + str(litter.generation_num) + " --------\n")
 
         save_population(litter)
-
         if len(litter.generation) <= 1:
             break
-
         litter.display_population()
         litter.rank_population()
         natural_selection(litter)
         litter.crossover()
+        litter.mutation()
         litter.generation_num += 1
 
     litter.optimal_tea()
